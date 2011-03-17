49d5c00daacf29b06160a76bb03d48a8702f7279
mooz <stillpedant@gmail.com>
2011-03-17 23:37:08 +0900

    add markup feature

--- percol/display.py
+++ percol/display.py
@@ -19,18 +19,150 @@
 
 import unicodedata
 import types
+import curses
 
-def display_len(s, beg = None, end = None):
-    if s.__class__ != types.UnicodeType:
-        return len(s)
+import markup, debug
 
-    if beg is None:
-        beg = 0
-    if end is None:
-        end = len(s)
+FG_COLORS = {
+    "black"   : curses.COLOR_BLACK,
+    "blue"    : curses.COLOR_BLUE,
+    "cyan"    : curses.COLOR_CYAN,
+    "green"   : curses.COLOR_GREEN,
+    "magenta" : curses.COLOR_MAGENTA,
+    "red"     : curses.COLOR_RED,
+    "white"   : curses.COLOR_WHITE,
+    "yellow"  : curses.COLOR_YELLOW,
+}
 
-    dlen = end - beg
-    for i in xrange(beg, end):
-        if unicodedata.east_asian_width(s[i]) in ("W", "F"):
-            dlen += 1
-    return dlen
+BG_COLORS = dict(("on_" + name, value) for name, value in FG_COLORS.iteritems())
+
+ATTRS = {
+    "altcharset" : curses.A_ALTCHARSET,
+    "blink"      : curses.A_BLINK,
+    "bold"       : curses.A_BOLD,
+    "dim"        : curses.A_DIM,
+    "normal"     : curses.A_NORMAL,
+    "standout"   : curses.A_STANDOUT,
+    "underline"  : curses.A_UNDERLINE,
+}
+
+COLORS = len(FG_COLORS)
+
+# ============================================================ #
+# Markup
+# ============================================================ #
+
+def get_fg_color(attrs):
+    for attr in attrs:
+        if attr in FG_COLORS:
+            return FG_COLORS[attr]
+    return FG_COLORS["white"]
+
+def get_bg_color(attrs):
+    for attr in attrs:
+        if attr in BG_COLORS:
+            return BG_COLORS[attr]
+    return BG_COLORS["on_black"]
+
+def get_attributes(attrs):
+    for attr in attrs:
+        if attr in ATTRS:
+            yield ATTRS[attr]
+
+# ============================================================ #
+# Display
+# ============================================================ #
+
+class Display(object):
+    def __init__(self, screen):
+        self.screen = screen
+        curses.start_color()
+        self.init_color_pairs()
+        self.parser = markup.MarkupParser()
+
+    # ============================================================ #
+    # Color Pairs
+    # ============================================================ #
+
+    def init_color_pairs(self):
+        for fg_s, fg in FG_COLORS.iteritems():
+            for bg_s, bg in BG_COLORS.iteritems():
+                if not (fg == bg == 0):
+                    curses.init_pair(self.get_pair_number(fg, bg), fg, bg)
+
+    def get_pair_number(self, fg, bg):
+        return fg + bg * COLORS
+
+    def get_color_pair(self, fg, bg):
+        return curses.color_pair(self.get_pair_number(fg, bg))
+
+    # ============================================================ #
+
+    def display_len(self, s, beg = None, end = None):
+        if s.__class__ != types.UnicodeType:
+            return len(s)
+
+        if beg is None:
+            beg = 0
+        if end is None:
+            end = len(s)
+
+        dlen = end - beg
+        for i in xrange(beg, end):
+            if unicodedata.east_asian_width(s[i]) in ("W", "F"):
+                dlen += 1
+        return dlen
+
+    def get_position_x(self, x_align, x_offset, whole_len):
+        position = 0
+        if x_align == "left":
+            position = x_offset
+        elif x_align == "right":
+            position = self.WIDTH - whole_len - x_offset
+        return position
+
+    def get_position_y(self, y_align, y_offset):
+        position = 0
+        if y_align == "top":
+            position = y_offset
+        elif y_align == "bottom":
+            position = self.HEIGHT - y_offset
+        return position
+
+    @property
+    def WIDTH(self):
+        return self.screen.getmaxyx()[1]
+
+    @property
+    def HEIGHT(self):
+        return self.screen.getmaxyx()[0]
+
+    def print_string(self, string,
+                     y_align = "top", y_offset = 0,
+                     x_align = "left", x_offset = 0):
+        tokens = self.parser.parse(string)
+
+        whole_len = reduce(lambda length, pair: length + len(pair[0]), tokens, 0)
+
+        position_x = self.get_position_x(x_align, x_offset, whole_len)
+        position_y = self.get_position_y(y_align, y_offset)
+
+        for s, attrs in tokens:
+            flag = self.get_color_pair(get_fg_color(attrs), get_bg_color(attrs))
+
+            for attr in get_attributes(attrs):
+                flag |= attr
+
+            try:
+                self.screen.addnstr(position_y, position_x, s, self.WIDTH - position_x, flag)
+            except Exception as e:
+                debug.log("Exception", e)
+
+            position_x += len(s)
+
+if __name__ == "__main__":
+    screen = curses.initscr()
+    display = Display(screen)
+    display.print_string("f<underline>oooo<red>ba</underline>aa</red>baz", x_offset = 10)
+    display.print_string("fo<on_green>ooo<red>ba<bold>a</on_green>a</bold></red>baz", x_align = "right", x_offset = 10)
+    screen.getch()
---
+++ percol/markup.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2011 mooz
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+class MarkupParser(object):
+    def __init__(self):
+        pass
+
+    def parse(self, s):
+        self.init_status(s)
+        self.parse_string()
+        self.consume_token()
+
+        return self.tokens
+
+    def init_status(self, s):
+        self.s = s
+        self.pos = 0
+        self.tokens = []
+        self.tags = []
+        self.buffer = []
+
+    def consume_token(self):
+        if self.buffer:
+            self.tokens.append(("".join(self.buffer), list(self.tags)))
+            self.buffer[:] = []
+
+    def get_next_char(self):
+        try:
+            p = self.pos
+            self.pos += 1
+            return self.s[p]
+        except IndexError:
+            return None
+
+    def get_next_chars(self):
+        s_len = len(self.s)
+        while self.pos < s_len:
+            yield self.get_next_char()
+
+    def peek_next_char(self):
+        try:
+            return self.s[self.pos]
+        except IndexError:
+            return None
+
+    def parse_string(self):
+        for c in self.get_next_chars():
+            if c == '<':
+                self.consume_token()
+
+                if self.peek_next_char() == '/':
+                    # end certain tag
+                    self.get_next_char()
+                    tag = self.parse_tag()
+
+                    try:
+                        self.tags.remove(tag)
+                    except:
+                        raise Exception("corresponding beginning tag for </{0}> is not found".format(tag))
+                else:
+                    # begin new tag
+                    tag = self.parse_tag()
+                    self.tags.insert(0, tag) # front
+            else:
+                self.buffer.append(c)
+
+    def parse_tag(self):
+        buf = []
+
+        for c in self.get_next_chars():
+            if c == '>':
+                return "".join(buf)
+            else:
+                buf.append(c)
+
+        raise Exception("Unclosed tag " + "".join(buf))
+
+if __name__ == "__main__":
+    import pprint, sys, types
+
+    parser = MarkupParser()
+
+    def color(str, color = 31):
+        colors = {
+            "black"      : 30,
+            "red"        : 31,
+            "green"      : 32,
+            "yellow"     : 33,
+            "blue"       : 34,
+            "magenta"    : 35,
+            "cyan"       : 36,
+            "white"      : 37,
+            "on_black"   : 40,
+            "on_red"     : 41,
+            "on_green"   : 42,
+            "on_yellow"  : 43,
+            "on_blue"    : 44,
+            "on_magenta" : 45,
+            "on_cyan"    : 46,
+            "on_white"   : 47,
+        }
+
+        if color.__class__ == types.StringType:
+            try:
+                color = colors[color]
+            except:
+                color = colors["white"]
+
+        if sys.stdout.isatty():
+            return "\033[1;{0}m{1}\033[0m".format(color, str)
+        else:
+            return str
+
+    tests = (
+        "hello",
+        "hello <red>red</red> normal",
+        "hello <on_green>with background green <bold>this is bold <red>and red</red></bold></on_green> then, normal",
+        "baaaaa<green>a<blue>aa</green>a</blue>aaaaaaa",
+        "baaaaa<green>a<blue>aa</blue>a</green>aaaaaaa",
+        u"マルチ<magenta>バイト<blue>文字</blue>の</magenta>テスト",
+    )
+
+    for test in tests:
+        try:
+            print("----------------------------------------------------------")
+            print("Testing [%s]" % color(test, "cyan"))
+            print(color("pass: " + pprint.pformat(parser.parse(test)), "green"))
+        except Exception as e:
+            print(color("fail: " + str(e), "red"))
