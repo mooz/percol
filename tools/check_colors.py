f1d778648c747af37dd12d247facfbae6ac85134
mooz <stillpedant@gmail.com>
2011-03-20 22:36:43 +0900

    display bold in check_colors.py

--- tools/check_colors.py
+++ tools/check_colors.py
@@ -39,14 +39,17 @@ if __name__ == "__main__":
                 if not (fg == bg == 0):
                     curses.init_pair(pair_number(fg, bg), fg, bg)
 
-        def print_pairs():
+        def print_pairs(attrs = None, offset_y = 0):
             fmt = " ({0}:{1}) "
             fmt_len = len(fmt)
 
             for bg, fg in get_fg_bg():
                 try:
                     color = curses.color_pair(pair_number(fg, bg))
-                    screen.addstr(bg, fg * fmt_len, fmt.format(fg, bg), color)
+                    if not attrs is None:
+                        for attr in attrs:
+                            color |= attr
+                    screen.addstr(offset_y + bg, fg * fmt_len, fmt.format(fg, bg), color)
                     pass
                 except curses.error:
                     pass
@@ -56,6 +59,7 @@ if __name__ == "__main__":
 
         init_pairs()
         print_pairs()
+        print_pairs([curses.A_BOLD], offset_y = curses.COLORS + 1)
         screen.refresh()
         wait_input()
     finally:
