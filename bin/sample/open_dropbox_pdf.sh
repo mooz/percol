0644cec9ecd81692cdba157aaf342fbfd3287dce
mooz <stillpedant@gmail.com>
2011-09-01 19:16:02 +0900

    Arrow spaces in file name

--- bin/sample/open_dropbox_pdf.sh
+++ bin/sample/open_dropbox_pdf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 gnome-terminal --title "Find PDF files"  --hide-menubar \
-    -e 'sh -c "evince `find ~/Dropbox/ -name \"*.pdf\" -type f | percol`"'
+    -e 'sh -c "evince \"$(find ~/Dropbox/ -name \"*.pdf\" -type f | percol)\""'
