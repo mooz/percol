594ea06e55427df1d87d8e1620461df653df2193
mooz <stillpedant@gmail.com>
2014-06-30 23:35:00 +0900

    Use stdout.buffer to output raw string in Python 3; refs #29

--- percol/actions.py
+++ percol/actions.py
@@ -17,23 +17,31 @@
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #
 
-import sys
+import sys, six
 
 from percol.action import action
 
 def double_quote_string(string):
     return '"' + string.replace('"', r'\"') + '"'
 
+def get_raw_stream(stream):
+    if six.PY2:
+        return stream
+    else:
+        return stream.buffer
+
 @action()
 def output_to_stdout(lines, percol):
     "output marked (selected) items to stdout"
+    stdout = get_raw_stream(sys.stdout)
     for line in lines:
-        sys.stdout.write(percol.display.get_raw_string(line))
-        sys.stdout.write("\n")
+        stdout.write(percol.display.get_raw_string(line))
+        stdout.write(six.b("\n"))
 
 @action()
 def output_to_stdout_double_quote(lines, percol):
     "output marked (selected) items to stdout with double quotes"
+    stdout = get_raw_stream(sys.stdout)
     for line in lines:
-        sys.stdout.write(percol.display.get_raw_string(double_quote_string(line)))
-        sys.stdout.write("\n")
+        stdout.write(percol.display.get_raw_string(double_quote_string(line)))
+        stdout.write(six.b("\n"))
