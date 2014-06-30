eb6d97a9b54ab5ac9255e75e0900ab8189d9954e
mooz <stillpedant@gmail.com>
2014-06-30 23:03:39 +0900

    Use latest six

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(name             = "percol",
                           "Topic :: Utilities"],
       keywords         = "anything.el unite.vim dmenu shell pipe filter curses",
       license          = "MIT",
-      install_requires = ["six"],
+      install_requires = ["six >= 1.7.3"],
       )
