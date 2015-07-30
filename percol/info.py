20d06d0693cd29f651dd2bb52d386093efa6e06b
Masafumi OYAMADA <stillpedant@gmail.com>
2015-07-30 17:30:46 +0900

    Version 0.2.0

--- percol/info.py
+++ percol/info.py
@@ -10,7 +10,7 @@ The interface of percol is highly inspired by anything.el for Emacs.
 
 Full details at https://github.com/mooz/percol"""
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 __logo__ = """                                    __
         ____  ___  ______________  / /
